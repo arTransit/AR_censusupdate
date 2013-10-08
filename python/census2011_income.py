4ed5673771fab53eaa897e08bb4fa8b29b297918
arTransit <andrew_ross@bctransit.com>
2013-10-08 10:28:42 -0700

    Census income started

--- python/census2011_income.py
+++ python/census2011_income.py
@@ -6,8 +6,9 @@
 # Tables are further cleaned and headers renamed.
 # 
 # Author: Andrew Ross
-# Date:   2013 September 30
+# Date:   2013 October 4
 #
+
 
 import sys
 import os
@@ -18,28 +19,48 @@ import csv
 provinceCodes = {'BC':59}
 geoLevels = [
         {'name':'Province', 'code':'PR', 'codeLength':2},
+        {'name':'City', 'code':'CITY', 'codeLength':3},
         {'name':'Census Division', 'code':'CD', 'codeLength':4},
         {'name':'Census Subdivision', 'code':'CSD', 'codeLength':7},
-        {'name':'Dissemination area', 'code':'DA', 'codeLength':8} ]
-columnHeaders= "PRUID,TTOTINC,TWTHOUTINC,TWTHINC,TUNDR5000,T5TO10000,T10TO15000,T15TO20000,T20TO25000,T30TO35000,T40TO50000,T50TO60000,T60TO80000,T80TO100,TOVER100,TMEDIAN,TAVERAGE"
-nullRecord= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-dataColumns = [
-        'Geography',
-        'Total - Total income in 2010',
-        'Without income',
-        'With income',
-        'Under $5,000',
-        '$5,000 to $9,999',
-        '$10,000 to $14,999',
-        '$15,000 to $19,999',
-        '$20,000 to $29,999',
-        '$30,000 to $39,999',
-        '$40,000 to $49,999',
-        '$50,000 to $59,999',
-        '$60,000 to $79,999',
-        '$80,000 to $99,999',
-        '$100,000 and over',
-        'Median income $',]
+        {'name':'Dissemination area', 'code':'DA', 'codeLength':8}
+    ]
+dataDictionary = [
+        ["UID",""],
+        ["GEOGRAPHY","Geography"],
+        ["TTOTINCPOP","Total - Total income in 2010"],
+        ["TWTHOUTINC","Without income"],
+        ["TWTHINC","With income"],
+        ["TUNDR5000","Under $5,000"],
+        ["T5TO10000","$5,000 to $9,999"],
+        ["T10TO15000","$10,000 to $14,999"],
+        ["T15TO20000","$15,000 to $19,999"],
+        ["T20TO30000","$20,000 to $29,999"],
+        ["T30TO40000","$30,000 to $39,999"],
+        ["T40TO50000","$40,000 to $49,999"],
+        ["T50TO60000","$50,000 to $59,999"],
+        ["T60TO80000","$60,000 to $79,999"],
+        ["T80TO100000","$80,000 to $99,999"],
+        ["TOVER100","$100,000 and over"],
+        ["TMEDIAN","Median income $"],
+        ["TAVERAGE","Average income $"],
+        ["FTOTINCFAM","Total - Economic family total income in 2010"],
+        ["FUNDER5","Under $5,000"],
+        ["F5TO10000","$5,000 to $9,999"],
+        ["F10TO15000","$10,000 to $14,999"],
+        ["F15TO20000","$15,000 to $19,999"],
+        ["F20TO30000","$20,000 to $29,999"],
+        ["F30TO40000","$30,000 to $39,999"],
+        ["F40TO50000","$40,000 to $49,999"],
+        ["F50TO60000","$50,000 to $59,999"],
+        ["F60TO80000","$60,000 to $79,999"],
+        ["F80TO100000","$80,000 to $99,999"],
+        ["FOVER100","$100,000 and over"],
+        ["FMEDIAN","Median family income $"],
+        ["FAVERAGE","Average family income $"]
+    ]
+columnHeaders= [d[0] for d in dataDictionary] #field names in destination file
+dataColumns = [d[1] for d in dataDictionary[1:] ] #field names in origin file
+
 
 def createOutputFiles( stub ):
     openFiles = []
@@ -53,7 +74,7 @@ def createOutputFiles( stub ):
 
 def writeHeader( fileList ):
     for f in fileList:
-        f.write( columnHeaders +'\n' )
+        f.write( ','.join( columnHeaders ) +'\n' )
 
 
 def formatData( d ):
@@ -68,20 +89,11 @@ def formatData( d ):
 
 
 def checkData( d ):
-    return True
     try:
-        total = int( float(d[1]))
-        s = sum( [int(float(i)) for i in d[2:]] )
-    except:
-        print ">>intfloatERROR: "
-        print d
+        float( d[1] )
+        return True
+    except ValueError:
         return False
-    else:
-        if abs(total - s) < 50:
-            return True
-        else:
-            print ">>difference: " + str( abs(total-s))
-            return False
 
 
 def getDictionaryFromCSV( inputFile ):
@@ -101,7 +113,7 @@ def getDictionaryFromCSV( inputFile ):
 
 
 def outputRecord( id, name, rA, outputFiles):
-    combinedRecord = [id, name, int( float(rA[0]) )] + rA
+    combinedRecord = [id, name ] + rA 
     for i in range(0, len(geoLevels)):
         if id < 10 ** geoLevels[i]['codeLength']:
             outputFiles[i].write( ','.join( [str(f) for f in combinedRecord] ) + '\n')
@@ -110,9 +122,9 @@ def outputRecord( id, name, rA, outputFiles):
 
 def processCensus( inputFiles, outputFiles ):
     #process input files as CSVs
-    [csvA] = [getDictionaryFromCSV( file ) for file in inputFiles]
+    csvA = getDictionaryFromCSV( inputFiles[0] )
 
-    for id in sorted( csvA.keys() ):
+    for id in csvA.keys():
         outputRecord( id, csvA[id][0], csvA[id][1:], outputFiles ) 
 
 
@@ -124,21 +136,21 @@ def closeFiles( fileList ):
 
 def getCommandLine():
     if (len(sys.argv) ==3):
-        if os.path.isfile( sys.argv[1] ):
+        if os.path.isfile( sys.argv[1] ) :
             return sys.argv[1],sys.argv[2]
     return "",""
 
 
 def printUsage():
     print "Usage:"
-    print "python " + sys.argv[0] + " <INPUT.csv> <OUTPUTSTUB>\n"
+    print "python " + sys.argv[0] + " <INCOME.csv> <OUTPUTSTUB>\n"
 
 
 if __name__ == "__main__":
-    print "Format Census 2011"
+    print "Format Census 2011: Language module"
 
     inputFileNameA,outputStub = getCommandLine()
-    if (not inputFileNameA):
+    if (not inputFileNameA) :
         printUsage()
         sys.exit(1)
 
